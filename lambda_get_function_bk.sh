d1ea775d188c5ded2c443e17d226d60e7702d3cb
atsushimemet <noap3b69n@gmail.com>
2024-12-25 18:34:40 +0900

    chore: add deploy.sh to create zip, backup, and deploy

--- lambda_get_function_bk.sh
+++
@@ -1,4 +0,0 @@
-aws lambda get-function \
-    --function-name vook_db_lambda_rakuten \
-    --query 'Code.Location' \
-    --output text
--- lambda_zipper.sh
+++
@@ -1,1 +0,0 @@
-zip -r vook_db_lambda_20241225.zip . -x ".env/*" -x ".git/*" -x "vook_db_lambda_rakuten-99999999-4338-4622-9ef9-1b4437cda8ed.zip" -x ".mypy_cache/*" -x "vook_db_v7/__pycache__/*" -x "*.zip" -x "notebook/*" -x "data/.DS_Store" -x "*/.ipynb_checkpoints/*"
