0cadcb299f2c5620cc65311b9ce1829a9103d6f3
atsushimemet <noap3b69n@gmail.com>
2024-12-26 17:06:57 +0900

    Feature issue 16 (#37)
    * add: update test data
    
    * update: Modify product_noise_judge_knowledge to include a process for filtering based on necessary keywords & modify df_judge to df_noise_judge
    
    * test: add case 1 for product_noise_judge_knowledge keyword (#16)
    
    * test: fix test data index & size_id
    
    * fix: corrected case-insensitive matching by using re.IGNORECASE flag
    
    * refactor: remove unused variables
    
    * add: update test data, BLACK
    
    * test: add assertion to check df_bulk_not_noise_ng_word has at least one record
    
    * docs: unify terminology from ユニットテスト to 単体テスト
    
    * chore: update deploy.sh to remove existing zip file, update echo messages, and exclude '-x *.zip'

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,32 @@
-# ZIPファイル名を変数に設定
-ZIP_FILE="vook_db_lambda_20241225_2.zip"
+#!/bin/bash
 
-# ZIPファイルを作成
-zip -r $ZIP_FILE . -x ".env/*" -x ".git/*" -x ".mypy_cache/*" -x "vook_db_v7/__pycache__/*" -x "*.zip" -x "notebook/*" -x "data/.DS_Store" -x "*/.ipynb_checkpoints/*"
+# 現在のディレクトリに存在するZIPファイルを削除
+echo "Deleting existing ZIP files in the current directory..."
+find . -maxdepth 1 -name "*.zip" -exec rm -v {} \;
+
+# 新しいZIPファイル名を設定
+ZIP_FILE="vook_db_lambda_20241226_1.zip"
+
+# 新しいZIPファイルを作成
+echo "Creating new ZIP file: $ZIP_FILE"
+zip -r $ZIP_FILE . \
+    -x ".env/*" \
+    -x ".git/*" \
+    -x ".mypy_cache/*" \
+    -x "vook_db_v7/__pycache__/*" \
+    -x "notebook/*" \
+    -x "data/.DS_Store" \
+    -x "*/.ipynb_checkpoints/*"
 
 # 関数コードの取得（オプション）
+echo "Getting Lambda function code location..."
 aws lambda get-function \
     --function-name vook_db_lambda_rakuten \
     --query 'Code.Location' \
     --output text
 
 # Lambda 関数コードの更新
+echo "Updating Lambda function code..."
 aws lambda update-function-code \
     --function-name vook_db_lambda_rakuten \
     --zip-file fileb://$ZIP_FILE
