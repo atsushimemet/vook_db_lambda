c7774b12d36853d4bc58132ecce0492c9f59e288
atsushimemet <noap3b69n@gmail.com>
2024-12-26 16:03:56 +0900

    test: add case 1 for product_noise_judge_knowledge keyword (#16)

--- tests/test_exclude_noise.py
+++ tests/test_exclude_noise.py
@@ -17,6 +17,12 @@ from tests.test_utils_10_2 import (
 from tests.test_utils_10_2 import (
     generate_mock_data_expected as generate_mock_data_expected_2,
 )
+from tests.test_utils_16_1 import (
+    generate_mock_data_actual as generate_mock_data_actual_16_1,
+)
+from tests.test_utils_16_1 import (
+    generate_mock_data_expected as generate_mock_data_expected_16_1,
+)
 from vook_db_v7.exclude_noise import (  # 実際のモジュール名に置き換え
     product_noise_judge_brand,
     product_noise_judge_knowledge,
@@ -99,3 +105,24 @@ class TestMainFunction(TestCase):
         self.assertIsInstance(df_bulk_not_noise_ng_word, pd.DataFrame)
         print("df_bulk_not_noise_ng_wordがDataFrameであることを確認")
         print("product_noise_judge_brand, knowledgeの正常終了を確認")
+
+
+def test_product_noise_judge_knowledge_keyword():
+    """知識レベルの必須キーワードが含まれていることをdf_bulkのname列を使用して確認"""
+    # テスト用データ
+    df_bulk_actual = generate_mock_data_actual_16_1()
+
+    # 関数を実行
+    df_cleaned = product_noise_judge_knowledge(df_bulk_actual)
+
+    # 期待される出力
+    df_bulk_expected = generate_mock_data_expected_16_1()
+
+    # name列をセットとして比較
+    actual_names = set(df_cleaned["name"])
+    expected_names = set(df_bulk_expected["name"])
+
+    # 集合が一致しているかを確認
+    assert (
+        actual_names == expected_names
+    ), f"Actual names: {actual_names}, Expected names: {expected_names}"
